5100fad32e24867b766480033a85cd8a18d17346
Jeet Viramgama <jeet.viramgama@levaral.com>
2022-08-18 21:29:44 +0530

    added a rewrite

--- next.config.js
+++ next.config.js
@@ -26,6 +26,14 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://thatmeme.site/api/:path*",
+      },
+    ];
+  },
 };
 
 const sentryWebpackPluginOptions = {
