a182fd94839f3749aac20eec9a9099716b3edb74
Jeet Viramgama <jeet.viramgama@levaral.com>
2022-08-18 14:34:52 +0530

    sentry dns open

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,9 @@ import * as Sentry from "@sentry/nextjs";
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn:
+    SENTRY_DSN ||
+    "https://07362048a37c4eb2b3129cec5664a1a5@o1365857.ingest.sentry.io/6662051",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
