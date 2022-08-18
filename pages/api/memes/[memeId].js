334845a6d98ce1578aedb9dcc443d8a1d694e972
Jeet Viramgama <jeet.viramgama@levaral.com>
2022-08-18 15:43:08 +0530

    possible fixes for allowing cors

--- pages/api/memes/[memeId].js
+++ pages/api/memes/[memeId].js
@@ -1,6 +1,26 @@
 import { connectToDatabase } from "../../../lib/mongodb";
 import { withSentry } from "@sentry/nextjs";
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 const handler = async (req, res) => {
   const ObjectId = require("mongodb").ObjectId;
 
@@ -30,4 +50,4 @@ const handler = async (req, res) => {
   }
 };
 
-export default withSentry(handler);
+export default withSentry(allowCors(handler));
