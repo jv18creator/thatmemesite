16f094b76d5e13b4247a4c65dba7289337ee128e
jv18creator <jviramgama5@gmail.com>
2022-10-12 18:24:36 +0530

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -29,69 +29,6 @@ const App = ({ Component, pageProps }) => {
 
   return (
     <>
-      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link
-        href={`https://fonts.googleapis.com/css2?family=Red+Hat+Display:wght@400;500;600;700;800;900&display=swap`}
-        rel="stylesheet"
-      ></link> */}
-      {/* Google Tag Manager */}
-      <Script
-        id="gtag-base"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', '${GTM_ID}');
-          `,
-        }}
-      />
-
-      {/* Google Adsense */}
-      {/* <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=NEXT_PUBLIC_ADSENSE_ID"
-        crossOrigin="anonymous"
-      ></script> */}
-      <Script
-        id="Adsense-id"
-        data-ad-client={process.env.NEXT_PUBLIC_ADSENSE_ID}
-        async
-        strategy="afterInteractive"
-        onError={(e) => {
-          console.error("Script failed to load", e);
-        }}
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      />
-      <script
-        async
-        custom-element="amp-auto-ads"
-        src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"
-      ></script>
-
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-
       <ChakraProvider theme={theme}>
         <UserProvider>
           <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -49,6 +49,26 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
 
         <body>
