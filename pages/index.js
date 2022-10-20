0d90554c47e63b5ceaac3b3541a11357bc41e75a
jv18creator <jviramgama5@gmail.com>
2022-10-20 13:43:51 +0530

    removed getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ import {
   MemePostsProvider,
 } from "../contexts/memePosts.context";
 import { UserContext } from "../contexts/user.context";
-import clientPromise from "../lib/mongodb";
 
 export default function Home() {
   const { fetchUser } = useContext(UserContext);
@@ -105,27 +104,4 @@ export default function Home() {
       </main>
     </div>
   );
-}
-
-export async function getServerSideProps(context) {
-  try {
-    await clientPromise;
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true },
-    };
-  } catch (e) {
-    console.error(e);
-    return {
-      props: { isConnected: false },
-    };
-  }
 }
